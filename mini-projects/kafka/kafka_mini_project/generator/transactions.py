f838008429c9c0e60d60ec0a02ac0fdfdd4f786d
rivergrove <anthony.olund@gmail.com>
2022-02-16 12:45:53 -0800

    fixed return statement

--- mini-projects/kafka/kafka_mini_project/generator/transactions.py
+++ mini-projects/kafka/kafka_mini_project/generator/transactions.py
@@ -7,14 +7,14 @@ account_chars: str = digits + ascii_letters
 def _random_account_id() -> str:
     """Return a random account number made of 12 characters."""
     return "".join(choices(account_chars, k=12))
-    
+
 def _random_amount() -> float:
     """Return a random amount between 1.00 and 1000.00."""
     return randint(100, 1000000) / 100
 
 def create_random_transaction() -> dict:
     """Create a fake, randomised transaction."""
-    retudrn {
+    return {
         "source": _random_account_id(),
         "target": _random_account_id(),
         "amount": _random_amount(),
