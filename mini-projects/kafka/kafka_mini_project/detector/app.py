7b1362f4a699f6519013388b63e457712fc3f759
rivergrove <anthony.olund@gmail.com>
2022-02-16 17:30:34 -0800

    update

--- mini-projects/kafka/kafka_mini_project/detector/app.py
+++ mini-projects/kafka/kafka_mini_project/detector/app.py
@@ -16,14 +16,14 @@ if __name__ == "__main__":
     consumer = KafkaConsumer(
         TRANSACTIONS_TOPIC,
         bootstrap_servers=KAFKA_BROKER_URL,
-        value_deserializer=lambda value: json.loads(value),
+        value_deserializer=lambda value: json.loads(value)
     )
     producer = KafkaProducer(
         bootstrap_servers=KAFKA_BROKER_URL,
-        value_serializer=lambda value: json.dumps(value).encode(),
+        value_serializer=lambda value: json.dumps(value).encode()
     )
     for message in consumer:
         transaction: dict = message.value
         topic = FRAUD_TOPIC if is_suspicious(transaction) else LEGIT_TOPIC
         producer.send(topic, value=transaction)
-        print(topic, transaction) # DEBUG
+        print(topic, transaction)  # DEBUG
--- mini-projects/kafka/kafka_mini_project/generator/transactions.py
+++ mini-projects/kafka/kafka_mini_project/generator/transactions.py
@@ -8,9 +8,11 @@ def _random_account_id() -> str:
     """Return a random account number made of 12 characters."""
     return "".join(choices(account_chars, k=12))
 
+
 def _random_amount() -> float:
     """Return a random amount between 1.00 and 1000.00."""
-    return randint(100, 1000000) / 100
+    return randint(100, 100000) / 100
+
 
 def create_random_transaction() -> dict:
     """Create a fake, randomised transaction."""
@@ -19,5 +21,6 @@ def create_random_transaction() -> dict:
         "target": _random_account_id(),
         "amount": _random_amount(),
         # Keep it simple: it's all dollars
-        "currency": "USD",
+        "currency": "USD"
     }
+
