d13d841de157e1f48d4552b9db318e069e0d2c9d
rivergrove <anthony.olund@gmail.com>
2022-02-18 12:25:05 -0800

    add db commands

--- mini-projects/flask/building_restful_apis_flask/app.py
+++ mini-projects/flask/building_restful_apis_flask/app.py
@@ -10,10 +10,52 @@ app.config['SQLAlCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, "plan
 
 db = SQLAlchemy(app)
 
+@app.cli.command('db_create')
+def db_create():
+    db.create_all()
+    print('Database created!')
+
+@app.cli.command('db_drop')
+def db_drop():
+    db.drop_all()
+    print('Databases dropped!')
+
+@app.cli.command('db_seed')
+def db_seed():
+
+    mercury = Planet(planet_name = 'mercury', 
+                     planet_type = 'Class D', 
+                     home_star = 'Sol',
+                     mass = 3.258e23,
+                     radius = 1516,
+                     distance = 35.98e6)
+
+    venus = Planet(planet_name = 'venus', 
+                     planet_type = 'Class K', 
+                     home_star = 'Sol',
+                     mass = 4.867e24,
+                     radius = 3760,
+                     distance = 67.24e6)
+
+    earth = Planet(planet_name = 'earth', 
+                     planet_type = 'Class M', 
+                     home_star = 'Sol',
+                     mass = 5.972e24,
+                     radius = 3959,
+                     distance = 92.96e6)
+
+    db.session.add(mercury)
+    db.session.add(venus)
+    db.session.add(earth)
+
+    test_user = User(first_name = 'Willy',
+                     last_name = 'Herschel',
+                     email = 'test@test.com',
+                     password = 'admin')
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
-
 
 @app.route('/super_simple')
 def super_simple():
