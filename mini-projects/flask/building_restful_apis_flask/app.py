725f76271fcf63ed24c2323ac85853d812f53938
rivergrove <anthony.olund@gmail.com>
2022-02-18 15:16:31 -0800

    output json to planets endpoint

--- mini-projects/flask/building_restful_apis_flask/app.py
+++ mini-projects/flask/building_restful_apis_flask/app.py
@@ -1,12 +1,15 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Float
+import json
 import os
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///planets.db'
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app) # needed to deserialize objects before passing them as json
 
 @app.cli.command('db_create')
 def db_create():
@@ -87,6 +90,12 @@ def url_variables(name: str, age: int):
     else:
         return jsonify(message=f"Welcome {name}!")
 
+@app.route('/planets',methods=['GET']) # methods tells which methods are allowed at this url
+def planets():
+    planets_list = Planet.query.all()
+    result = planets_schema.dump(planets_list)
+    return jsonify(result)
+
 # Database models. 
 # We could split this out into different files using the modular form of python.
 class User(db.Model):
@@ -107,6 +116,21 @@ class Planet(db.Model):
     mass = Column(Float)
     radius = Column(Float)
     distance = Column(Float)
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'first_name', 'last_name', 'email', 'password')
+
+class PlanetSchema(ma.Schema):
+    class Meta:
+        fields = ('planet_id', 'planet_name', 'planet_type', 'home_star', 'mass', 'radius', 'distance')
+
+# instantiate 2 different versions of each class
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+planet_schema = PlanetSchema()
+planets_schema = PlanetSchema(many=True)
 
 
 if __name__ == '__main__':
