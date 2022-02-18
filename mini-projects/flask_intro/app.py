9d273dd1805fe77c1a40614eea6397b33f2dd4b8
rivergrove <anthony.olund@gmail.com>
2022-02-17 19:22:53 -0800

    now working

--- mini-projects/flask_intro/app.py
+++ mini-projects/flask_intro/app.py
@@ -45,15 +45,15 @@ def delete(id):
     except:
         return 'There was a problem deleting that task'
 
-# @app.route('/update/<int:id>', methods=['GET', 'POST'])
-# def update(id):
-#     task = Todo.query.get_or_404(id)
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
 
-#     if request.method == 'POST':
-#         pass
-#     else:
-#         return render_template('update.html', task=task)
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('update.html', task=task)
 
 
 if __name__ == "__main__":
-    app.run(debug=True) #, port=5000) 
+    app.run(debug=True, port=5000) 
