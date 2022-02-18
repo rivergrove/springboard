15e7793dd6bddaff1ed6a1e82f02ced735fd21b0
rivergrove <anthony.olund@gmail.com>
2022-02-18 15:34:24 -0800

    added register new users

--- mini-projects/flask/building_restful_apis_flask/app.py
+++ mini-projects/flask/building_restful_apis_flask/app.py
@@ -96,6 +96,21 @@ def planets():
     result = planets_schema.dump(planets_list)
     return jsonify(result)
 
+@app.route('/register', methods=['POST'])
+def register():
+    email = request.form['email']
+    test = User.query.filter_by(email=email).first()
+    if test:
+        return jsonify(message='That email already exists'), 409
+    else:
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        password = request.form['password']
+        user = User(first_name=first_name, last_name=last_name, email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify(message="user created successfully"), 201
+
 # Database models. 
 # We could split this out into different files using the modular form of python.
 class User(db.Model):
