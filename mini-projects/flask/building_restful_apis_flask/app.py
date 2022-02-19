5cceff6f29985fbcf106e6982a08f08b8fa3d248
rivergrove <anthony.olund@gmail.com>
2022-02-18 18:02:19 -0800

    added add planet function

--- mini-projects/flask/building_restful_apis_flask/app.py
+++ mini-projects/flask/building_restful_apis_flask/app.py
@@ -156,6 +156,28 @@ def planet_details(planet_id: int):
     else:
         return jsonify(message="that planet does not exist"), 404
 
+@app.route('/add_planet', methods=['POST'])
+def add_planet():
+    planet_name = request.form['planet_name']
+    test = Planet.query.filter_by(planet_name=planet_name).first()
+    if test:
+        return jsonify(message="There was already a planet by that name"), 409
+    else:
+        planet_type = request.form['planet_type']
+        home_star = request.form['home_star']
+        mass = request.form['mass']
+        radius = request.form['radius']
+        distance = request.form['distance']
+
+        new_planet = Planet(planet_name=planet_name,
+                            planet_type=planet_type,
+                            home_star=home_star,
+                            mass=mass,
+                            radius=radius,
+                            distance=distance)
+        db.session.add(new_planet)
+        db.session.commit()
+        return jsonify(message="Planet added"), 201
 
 # Database models. 
 # We could split this out into different files using the modular form of python.
